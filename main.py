b5cf5287a5e67fb1e2022f796b08125b168ca925
Arnav Nagpurkar <84492896+ArnavNagpurkar@users.noreply.github.com>
2023-04-27 17:06:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import win32com.client as wincom
+
+speak = wincom.Dispatch("SAPI.SpVoice")
+
+version = 1.01
+print(f"Welcome to Robo Speaker {version}")
+while True:
+    userInp = input("What you want to speak or else type \"00\" to exit: ")
+    if userInp == "00":
+        speak.Speak("Bye Bye!")
+        print(f"Thanks for using Robo Speaker {version}")
+        break
+    command = speak.Speak(userInp)
+
 
